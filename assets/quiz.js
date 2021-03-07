7fec57e038a1e5f9ce89215c5d273e5452582ee5
xvirix <77359420+xvirix@users.noreply.github.com>
2021-03-06 23:41:54 -0600

    Add files via upload

---
+++ assets/quiz.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+      title: "An array’s length can be evaluated with the what property?",
+      multiChoice: [".length", ".log", "the console", ".loop"],
+      answer: ".length"
+    },
+  
+    {
+      title: "Within a loop, the 'break' keyword may be used to do what?",
+      multiChoice: ["break your competitors code", "exit the loop immediately", "repeat the loop", "indicate a stopping condition"],
+      answer: "exit the loop immediately"
+    },
+  
+    {
+      title: "Properties in a JavaScript oject are often refferred to as what?",
+      multiChoice: ["dot walking", "key-value pairs", "nested properties", "undefined"],
+      answer: "key-value pairs"
+    },
+  
+    {
+      title: "Which array method inserts an element at the end of the array?",
+      multiChoice: [".pop()", ".push()", ".length", ".join()"],
+      answer: ".push()"
+    },
+  
+    {
+      title: "What is a callback function?",
+      multiChoice: ["a function that accepts an array as an argument", "I function that performs an HTTP request", "a data type similar to a string or a boolean", "a function that is passed into another function as an argument"],
+      answer: "a function that is passed into another function as an argument"
+    }
+  ];
+  
